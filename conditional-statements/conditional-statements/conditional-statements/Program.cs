fe307071ddf2c76a5604fa472ee07c7f1013c32b
juhomontonen <juhop.montonen@gmail.com>
2017-09-21 15:45:16 +0300

    First update

---
+++ conditional-statements/conditional-statements/conditional-statements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditional_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber; 
+            int.TryParse(userInput, out evaluatedNumber);
+
+            if(evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");
+            }
+            if(evaluatedNumber > 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
+            }
+            else
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
