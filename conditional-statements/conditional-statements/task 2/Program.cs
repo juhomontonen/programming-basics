3d33421aa6d1f138b08aa8d135c0ae09414bd6f7
juhomontonen <juhop.montonen@gmail.com>
2017-09-22 13:41:32 +0300

    Added task 2

---
+++ conditional-statements/conditional-statements/task 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define instructions
+            Console.WriteLine("Ohjelma selvittää onko syötetty luku parillinen vai pariton.");
+            bool isNumber;
+            Console.Write("Syötä luku: ");
+            // Define variables
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+            // program logic
+            if (evaluatedNumber % 2 == 0)
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on parillinen.");
+            }
+            else
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on pariton");
+            }
+
+                    Console.ReadKey();
+         }
+    }
+}
