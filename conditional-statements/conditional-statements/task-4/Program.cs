aef592f67ca59e63d412f4276de9dcb7b336c03c
juhomontonen <juhop.montonen@gmail.com>
2017-09-29 09:28:03 +0300

    Edited task 4

--- conditional-statements/conditional-statements/task-4/Program.cs
+++ conditional-statements/conditional-statements/task-4/Program.cs
@@ -20,35 +20,45 @@ namespace task_4
                 int numx = int.Parse(numero1);
 
                 Console.WriteLine("Syötä numero 2: ");
-                // Define variables
                 string numero2 = Console.ReadLine();
                 int numy = int.Parse(numero2);
 
                 Console.WriteLine("Syötä numero 3: ");
-                // Define variables
                 string numero3 = Console.ReadLine();
                 int numz = int.Parse(numero3);
 
                 if (numx < numy && numx < numz)
                 {
                     if (numy < numz)
+                    {
                         Console.WriteLine($"Järjestys on {numx}, {numy}, {numz}");
+                    }
                     else
-                        Console.WriteLine($"Järjestys on {numx}, {numz}, {numy}");
+                    { 
+                    Console.WriteLine($"Järjestys on {numx}, {numz}, {numy}");
+                    }   
                 }
                 if (numy < numx && numy < numz)
                 {
                     if (numx < numz)
+                    {
                         Console.WriteLine($"Järjestys on {numy}, {numx}, {numz}");
+                    }
                     else
+                    {
                         Console.WriteLine($"Järjestys on {numy}, {numz}, {numx}");
+                    }
                 }
                 if (numz < numx && numz < numy)
                 {
                     if (numx < numy)
+                    {
                         Console.WriteLine($"Järjestys on {numz}, {numx}, {numy}");
+                    }
                     else
+                    {
                         Console.WriteLine($"Järjestys on {numz}, {numy}, {numx}");
+                    }
                 }
                 Console.ReadKey();
             }
