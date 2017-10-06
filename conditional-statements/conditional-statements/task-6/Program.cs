fa5040d7d32d8aef031ccb44b1773bba2baf2db3
juhomontonen <juhop.montonen@gmail.com>
2017-10-06 15:38:48 +0300

    Added task 7

---
+++ conditional-statements/conditional-statements/task-6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä ohjelmaan luku: ");
+            // Define variables
+            string luku = Console.ReadLine();
+            int num = int.Parse(luku);
+            int i = 1;
+            int n = 1;
+            string parseOutput = "";
+
+            while (i <= num)
+            {
+                parseOutput = $"{parseOutput} x {i}";
+                n = n * i;              
+                Console.WriteLine($"{i}!={n}"); //5!=120
+                i++;
+            }
+            Console.WriteLine($"{parseOutput}={n}");
+            Console.ReadLine();
+        }
+    }
+}
