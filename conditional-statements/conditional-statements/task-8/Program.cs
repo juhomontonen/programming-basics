658f945a3e8c83370d604a5f5ec95686da7bd136
juhomontonen <juhop.montonen@gmail.com>
2017-10-20 10:26:12 +0300

    Added task 8 and task 9.

---
+++ conditional-statements/conditional-statements/task-8/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä luku. Ohjelma laskee parillisten ja parittomien summan: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            int i = 1;
+            int even = 0;
+            int odd = 0;
+
+            do
+            {
+                if (i % 2 == 0)
+                    even += i;
+                else
+                    odd += i;
+                i++;
+            }
+            while (i <= number);
+
+            Console.WriteLine($"Parillisten lukujen summa = {even} ja parittomien lukujen summa = {odd}");
+
+         Console.ReadKey();
+        }
+    }
+}
---
+++ conditional-statements/conditional-statements/task-9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee lisäyksen. Anna luku, jonka lisäys lasketaan: ");
+            string userInput = Console.ReadLine();
+
+            int number = int.Parse(userInput);
+
+            int i = 1;
+            int f = 0;
+
+            if (number == 0)
+            {
+                Console.WriteLine("Virheellinen syöte");
+            }
+            else if (number > 0)
+            {
+                do
+                {
+                    i = i + 1;
+
+                    f = f + i;
+                    Console.WriteLine($"{i} = {f}");
+                } while (i < number);
+            }
+            else if (number < 0)
+            {
+                do
+                {
+                    i = i - 1;
+
+                    f = f + i;
+                    Console.WriteLine($"{i} = {f}");
+                } while (i > number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
