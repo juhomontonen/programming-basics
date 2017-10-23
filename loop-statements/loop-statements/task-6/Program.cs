da082201257e568d330966ef6d84326aedf7e345
juhomontonen <juhop.montonen@gmail.com>
2017-10-23 11:56:24 +0300

    Added task 6.

---
+++ loop-statements/loop-statements/task-6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ohjelma simuloi nopanheittoa 1000 kertaa.\n");
+            Console.Write("Lisäksi ohjelma kertoo numeron 6 esiintymiskertojen lukumäärän.\n");
+
+            Random rnd = new Random();
+            int six = 0;
+
+            for (int i = 1;i<=1000;i++)
+            {
+                Console.WriteLine($"{i}. {rnd.Next(6)}");
+                if (rnd.Next(6) == 1)
+                {
+                    six += 1;
+                }
+            }
+            Console.WriteLine($"Kuutonen arvottiin {six} kertaa.");
+            Console.ReadKey();
+        }
+    }
+}
