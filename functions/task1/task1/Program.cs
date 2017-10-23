8cbeea759713fc5376ce80cf0ce9f7182c4c9c07
juhomontonen <juhop.montonen@gmail.com>
2017-10-23 12:27:32 +0300

    Added the first task to functions.

---
+++ functions/task1/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            do
+            {
+                Console.Write("Syötä ohjelmalle luku: ");
+                string luku = Console.ReadLine();
+                num = int.Parse(luku);
+                if (num < 0)
+                {
+                    Console.Write($"Numero {num} ei ole sallittu luku.\n");
+                }
+
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write("*");
+                }
+            }
+            while (num < 0);
+            Console.ReadKey();
+        }
+    }
+}
