b23eee67ce4c913ae4cde71c0e5c3bd196d47326
juhomontonen <juhop.montonen@gmail.com>
2017-10-23 11:11:40 +0300

    Added task 5.

---
+++ loop-statements/loop-statements/task-4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ohjelma heittää kolikkoa.\nKerro kuinka monta kertaa haluat kolikkoa heitettävän: ");
+            string luku = Console.ReadLine();
+            int num = int.Parse(luku);
+            int kruuna = 0;
+            int klaava = 0;
+            Random rnd = new Random();
+
+            for (int i=1;i<= num;i++)
+            {
+                if (rnd.Next(2) == 1)
+                {
+                    klaava += 1;
+                }
+                else
+                {
+                    kruuna += 1;
+                }
+            }
+            Console.Write($"Kolikkoa on heitetty {num} kertaa. \n");
+            Console.WriteLine($"Kruunia tuli {kruuna} ja klaavoja {klaava}.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
