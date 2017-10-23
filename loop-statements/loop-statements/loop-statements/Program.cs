ac4f58f795b54c95785c021ed47b237abc63029d
juhomontonen <juhop.montonen@gmail.com>
2017-10-23 10:03:20 +0300

    First task done in loop statements.

---
+++ loop-statements/loop-statements/loop-statements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa 1-10 kokonaisluvut sekä niiden neliöjuuret: ");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} \t {Math.Sqrt(i):f2}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
