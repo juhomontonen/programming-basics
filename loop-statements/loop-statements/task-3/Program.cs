d8a877a6c1edc1e7af2ba1afac0a988b8f2aa5a4
juhomontonen <juhop.montonen@gmail.com>
2017-10-23 10:31:26 +0300

    Added task 3 to the loop statements.

---
+++ loop-statements/loop-statements/task-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua väliltä 0-50: ");
+            Random rnd = new Random();
+
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine($"{rnd.Next(50)},\t {rnd.Next(50)},\t {rnd.Next(50)},\t {rnd.Next(50)},\t {rnd.Next(50)}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
