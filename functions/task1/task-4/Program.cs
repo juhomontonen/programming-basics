545f218049b5e5cd42cb6705e8b47cd07e60e7d8
juhomontonen <juhop.montonen@gmail.com>
2017-10-24 12:03:30 +0300

    Added functions task 4.

---
+++ functions/task1/task-4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä ohjelmaan 10 positiivista kokonaislukua. Ohjelma palauttaa suurimman.");
+            Console.WriteLine(Luvut());
+            Console.ReadKey();
+        }
+        static string Luvut()
+        {
+            int luku = 0;
+            bool num = false;
+            int iso = 0;
+            int mones = 0;
+            string lukujono = "";
+
+            for (int i = 1; i <= 10; i++)
+            {
+                do
+                {
+                    Console.Write($"Syötä {i}. luku: ");
+                    num = int.TryParse(Console.ReadLine(), out luku);
+                    if (!num || luku < 1)
+                    {
+                        Console.WriteLine($"{luku} ei ole positiivinen kokonaisluku.");
+                    }
+                    if (iso < luku)
+                    {
+                        iso = luku;
+                        mones = i;
+                    }
+
+                }
+                while (luku < 0);
+
+                lukujono += $"{luku} ";
+
+            }
+            Console.WriteLine($"Syötit seuraavat luvut: {lukujono}\n");
+            return ($"Suurin luku oli {iso} eli syöte numero {mones}.");
+        }
+    }
+}
