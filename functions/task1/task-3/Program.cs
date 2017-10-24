b533df56db8eda73334fef3b75f24e08fcd5b79a
juhomontonen <juhop.montonen@gmail.com>
2017-10-24 10:33:17 +0300

    Finished functions task 3.

--- functions/task1/task-3/Program.cs
+++ functions/task1/task-3/Program.cs
@@ -10,39 +10,30 @@ namespace task_3
     {
         static void Main(string[] args)
         {
-            int n = 0;
-            int ok = 0;
-            int lowerBound = 1;
-            int upperBound = 20;
-            while (true)
-            {
-                Console.Write("Syötä luku väliltä 1-20");
-                n = int.Parse(Console.ReadLine());
-
-                if (n < 1 && n > 20)
-                {
-                    Console.WriteLine($"Luku {n} ei ole sallittu luku.");
-                }
-                else
-                {
-                    break;
-                }
-                
-            }
+            Console.WriteLine("Ohjelma tarkistaa onko luku annetulta alueelta.");
             int retNumber = numberFromRange(1, 20);
+            Console.WriteLine($"Syötit {retNumber}. Se on annetulta alueelta.");
+            Console.ReadKey();
 
         }
         static int numberFromRange(int lowerBound, int upperBound)
         {
-            if (n >= lowerBound && n <= upperBound)
+            int luku = 0;
+            bool num = false;
+            do
             {
-                return ok;
-            }
-            else
-            {
+                Console.Write($"Syötä luku väliltä {lowerBound}-{upperBound}: ");
+                num = int.TryParse(Console.ReadLine(), out luku);
+                if (!num || luku < lowerBound || luku > upperBound)
+                {
+                    Console.WriteLine($"Syötit {luku}. Se ei ole annetulta alueelta.");
+                }
 
+            }
+            while (!num || luku < lowerBound || luku > upperBound);
+            {
+                return luku;
             }
         }
-
     }
 }
