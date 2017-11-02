eb3fed50ed47523bd1ba23e75fca807eadecf9d5
juhomontonen <juhop.montonen@gmail.com>
2017-11-02 13:34:08 +0200

    Added first task to arrays.

---
+++ arrays/arrays/arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNumber = new int[100];
+            Random rnd = new Random();
+            double sum = 0;
+
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                arrayNumber[i] = rnd.Next(0, 51);
+                Console.WriteLine(arrayNumber[10]);
+            }
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                Console.WriteLine($"{i}. {arrayNumber[i]}");
+                sum += arrayNumber[i];
+
+            }
+            Console.WriteLine($"Lukujen summa on {sum}");
+            Console.WriteLine($"Lukujen keskiarvo on {sum/arrayNumber.Length:f2}");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
