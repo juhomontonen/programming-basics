db009e529179dca0e297d02cda08f09fbdd152f4
juhomontonen <juhop.montonen@gmail.com>
2017-11-10 11:00:03 +0200

    Started arrays task 2. Not complete.

---
+++ arrays/arrays/task-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lottorivi = new int[8];
+            Random rnd = new Random();
+
+
+                for (int i = 0; i < lottorivi.Length; i++)
+                {
+                    lottorivi[i] = rnd.Next(0, 40);
+                }
+
+            Console.WriteLine($"{lottorivi[1]} {lottorivi[2]} {lottorivi[3]} {lottorivi[4]}");
+
+
+                Console.ReadKey();
+        }
+    }
+}
