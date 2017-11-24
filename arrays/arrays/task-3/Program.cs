f8490453766204adfd41c6fd3478b68e7d6a151c
juhomontonen <juhop.montonen@gmail.com>
2017-11-24 10:59:15 +0200

    Initial commit to arrays task 3.

---
+++ arrays/arrays/task-3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjema arpoo yksiulotteiseen kokonaislukutaulukkoon arvot väliltä 0-20");
+            int[] arrayNumber = new int[10];
+            Random rnd = new Random();
+            Console.WriteLine("[X]\t=\tArvo");
+
+            for (int i = 0; i < 10; i++)
+            {
+                arrayNumber[i] = rnd.Next(0, 20);
+                if (arrayNumber[i] < 10)
+                {
+                    Console.WriteLine($"[{i}]\t=\t0{arrayNumber[i]}");
+                }
+                else
+                {
+                    Console.WriteLine($"[{i}]\t=\t{arrayNumber[i]}");
+                }
+
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
