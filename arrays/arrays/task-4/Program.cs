6d62df956f2128521df923260ed64c825a38db53
juhomontonen <juhop.montonen@gmail.com>
2017-11-24 11:42:36 +0200

    Added arrays task 4.

---
+++ arrays/arrays/task-4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjema arpoo kaksiulotteisen kokonaislukutaulukkoon arvot väliltä 0-100");
+            int[,] arrayNumber = new int[10,20];
+            Random rnd = new Random();
+            Console.WriteLine("[X, Y] = Arvo ");
+
+            for (int i=0;i<10;i++)
+            {
+                for (int y=0;y<20;y++)
+                {
+                    arrayNumber[i, y] = rnd.Next(0, 100);
+                    if (arrayNumber[i, y] < 10)
+                    {
+                        Console.WriteLine($"[{i}, {y}] = 0{arrayNumber[i,y]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"[{i}, {y}] = {arrayNumber[i, y]}");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ arrays/arrays/task-4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("task-4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Saimaan AMK")]
+[assembly: AssemblyProduct("task-4")]
+[assembly: AssemblyCopyright("Copyright © Saimaan AMK 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a22e8535-71f3-46c9-af70-6fd133e9b9b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
