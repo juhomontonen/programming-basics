d250a4d50a9e098094f89bb72795e5a6768394ab
juhomontonen <juhop.montonen@gmail.com>
2017-11-30 11:22:27 +0200

    Initial commit to string handling.

---
+++ string-handling/string-handling/string-handling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kirjoita jotain: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            Console.WriteLine($"Tekstissäsi on {userInput.Length} merkkiä");
+
+            Console.ReadKey();
+        }
+    }
+}
