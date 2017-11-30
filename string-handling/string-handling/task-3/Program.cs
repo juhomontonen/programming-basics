1ff5c62651d31a18acc56b5155194666df384683
juhomontonen <juhop.montonen@gmail.com>
2017-11-30 12:35:21 +0200

    Added task 3.

---
+++ string-handling/string-handling/task-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kirjoita jotain: ");
+            string userInput;
+            userInput = Console.ReadLine().ToUpper();
+            int i = 0;
+
+            foreach (char x in userInput)
+            {
+                if (x == 'L')
+                i++;
+            }
+
+
+            Console.WriteLine($"Tekstissäsi on {i} L-merkkiä.");
+            Console.ReadKey();
+        }
+    }
+}
