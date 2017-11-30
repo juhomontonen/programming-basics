a7be7c9165364db01dc2ee24101fbd749cf4fd4d
juhomontonen <juhop.montonen@gmail.com>
2017-11-30 12:03:22 +0200

    Added string handling task 2.

---
+++ string-handling/string-handling/task-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kirjoita jotain: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+
+            Console.WriteLine($"{userInput.Replace("e","@")}");
+            Console.ReadKey();
+        }
+    }
+}
