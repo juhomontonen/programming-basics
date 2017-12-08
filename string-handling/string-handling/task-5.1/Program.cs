fe614fcecf68e4f697494d3dd473cffb48924f29
juhomontonen <juhop.montonen@gmail.com>
2017-12-08 11:06:23 +0200

    Added task 5 and 5.1 to string handling.

---
+++ string-handling/string-handling/task-5.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kirjoita jotain: ");
+            string userInput;
+            userInput = Console.ReadLine().ToUpper();
+            int maara = 0;
+            string vokaalit = "AEIOUYÄÖ";
+
+            
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                for(int j=0;j<vokaalit.Length;j++)
+                {
+                    if (vokaalit[j] == userInput[i])
+                    {
+                        maara++;
+                        break;
+                    }
+                }
+            }
+
+
+            Console.WriteLine($"Tekstissäsi {userInput} on {maara} vokaalia.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ string-handling/string-handling/task-5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kirjoita jotain: ");
+            string userInput;
+            userInput = Console.ReadLine().ToUpper();
+            int maara = 0;
+
+            char[] vokaalit = new char[] { 'A', 'E', 'I', 'O', 'U', 'Y', 'Ä', 'Ö' };
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                foreach (char x in vokaalit)
+                {
+                    if (x == userInput[i])
+                    {
+                        maara++;
+                        break;
+                    }
+                }
+            }
+
+
+            Console.WriteLine($"Tekstissäsi {userInput} on {maara} vokaalia.");
+            Console.ReadKey();
+
+        }
+    }
+}
